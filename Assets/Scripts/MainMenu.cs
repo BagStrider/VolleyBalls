0fffa6cafb68643744eb4bd9572233c964df8b68
Andrey Kanuhnevich <andrey64931@gmail.com>
2022-04-15 21:24:48 +0300

    Added settings scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,6 +15,6 @@ public class MainMenu : MonoBehaviour
 
     public void OpenSettings()
     {
-
+        SceneManager.LoadScene(2);
     }
 }
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
